fbcb5bd3d42658016216b4c0854903fc2a558b9d
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-15 19:19:35 +0100

    Create File class

---
+++ src/main/java/models/File.java
@@ -0,0 +1,59 @@
+package models;
+
+public class File {
+    private int id;
+    private String name;
+    private String extension;
+    private Double size;
+    private Folder folder;
+
+    public File() {
+    }
+
+    public File(String name, String extension, Double size, Folder folder) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+        this.folder = folder;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+}
