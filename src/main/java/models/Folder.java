8696397f4d303ec027f25b36755af783c173dc4e
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-15 19:20:46 +0100

    Create Folder class

---
+++ src/main/java/models/Folder.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.Set;
+
+public class Folder {
+    private int id;
+    private String title;
+    private Set<File> files;
+
+    public Folder() {
+    }
+
+    public Folder(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(Set<File> files) {
+        this.files = files;
+    }
+}
